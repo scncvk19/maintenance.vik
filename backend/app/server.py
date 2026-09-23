ef072093e11bdc7029cad112e4aa28be78ce051a
Sercan | OWA-Lab <176288592+scncvk19@users.noreply.github.com>
2026-09-23 09:51:02 +0200

    Expose authentication API

--- backend/app/server.py
+++ backend/app/server.py
@@ -3,7 +3,9 @@ from .main import app
 from .telegram_reminders import router as telegram_router
 from .document_analysis import router as document_analysis_router
 from .document_category import router as document_category_router
+from .auth import router as auth_router
 
 app.include_router(telegram_router)
 app.include_router(document_analysis_router)
 app.include_router(document_category_router)
+app.include_router(auth_router)
